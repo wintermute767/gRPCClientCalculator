81a3938593836097f448c3e7f392f5854ae05915
Wintermute766 <wintermute766@gmail.com>
2022-11-21 11:03:03 +0300

    fix

--- cmd/myclient.go
+++ cmd/myclient.go
@@ -10,25 +10,21 @@ import (
 
 func main() {
 	for {
+
 		//читаем построчно команды
-		reader := bufio.NewReader(os.Stdin)
-		if reader != nil {
-			newstring, err := reader.ReadString('\n')
-			if err != nil {
-				log.Printf("can not read: %s\n", err)
-
-				//ищем параметры
-				ipAddres, par1, par2, err := requestparameters.GetRequestParameters(newstring)
-				if err != nil {
-					log.Printf("can not read: %s\n", err)
-					continue
-				}
-				//Делаепм запрос на gRPC сервер
-				answerfromserv.GetAnswerFromServer(ipAddres, par1, par2)
-
-			}
-
+		reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Printf("can not read: %s\n", err)
 		}
+		//ищем параметры
+		ipAddres, par1, par2, err := requestparameters.GetRequestParameters(reader)
+		if err != nil {
+			log.Printf("can not read: %s\n", err)
+			continue
+		}
+		//Делаепм запрос на gRPC сервер
+		answerfromserv.GetAnswerFromServer(ipAddres, par1, par2)
 
 	}
+
 }
