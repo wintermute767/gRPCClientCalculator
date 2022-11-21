c1ab95e241d611361246f1e02659db8a2a816b49
Wintermute766 <wintermute766@gmail.com>
2022-11-21 05:54:04 +0300

    fix

--- pkg/requestparameters/requestparameters.go
+++ pkg/requestparameters/requestparameters.go
@@ -8,7 +8,7 @@ import (
 
 func GetRequestParameters(str string) (string, float32, float32, error) {
 	//регулярное выражение чтобы найти группы ip адрес сервера первое и второе число
-	reg := regexp.MustCompile(`(\d+.\d+.\d+.\d+:\d+)\s(\d+.*\d*)\s(\d+.*\d*)(\s*\r\n)`)
+	reg := regexp.MustCompile(`(\d+.\d+.\d+.\d+:\d+)\s(\d+.*\d*)\s(\d+.*\d*)\s*\r*\n`)
 	strGroup := reg.FindSubmatch([]byte(str))
 	if strGroup == nil {
 		return "", 0, 0, errors.New("failed to read the parameters, need <ip-address>:<port> <float number 1> <float number 2>")
